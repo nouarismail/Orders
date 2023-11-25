d8a0225e6c2d14af41882e1b5d45da8a1c6679b3
nouarismail <nouaresmail@gmail.com>
2023-11-25 10:33:12 +0300

    localization ru

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Orders.Data;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 var builder = WebApplication.CreateBuilder(args);
 // builder.Services.AddDbContext<OrdersApplicationDbContext>(options =>
 //     options.UseSqlite(builder.Configuration.GetConnectionString("OrdersApplicationDbContext") ?? throw new InvalidOperationException("Connection string 'OrdersApplicationDbContext' not found.")));
@@ -12,7 +15,23 @@ builder.Services.AddDbContext<OrdersApplicationDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc()
+        .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+        .AddDataAnnotationsLocalization();
+builder.Services.Configure<RequestLocalizationOptions>(options=>
+{
+    var supportedCultrues =new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("ru-RU")
+    };
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedUICultures = supportedCultrues;
+});
+
 var app = builder.Build();
+app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
